3bb2b458d76b0a187bbe1ae1f17bafa27223e25f
Haunted Loaf <haunted.loaf@gmail.com>
2022-05-20 15:30:58 +0100

    Convert built-ins to pure JSON

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import './style.less'
 
 import App from './App.svelte'
-import { State } from "./lib/State"
-import { builtin } from './builtin'
+import { State, StateDumpV1 } from "./lib/State"
+import builtin from './builtin.json'
 
 const state = new State()
 state.init()
 state.start()
-state.load(builtin, false, false)
+state.load(builtin as StateDumpV1, false, false)
 
 const app = new App({
   target: document.querySelector('#app'),
